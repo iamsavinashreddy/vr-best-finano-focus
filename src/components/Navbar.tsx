9355ad8330a97da8571a30a93e80ddfbbba8dd7a
iamsavinashreddy <iamsavinashreddy@gmail.com>
2025-04-09 12:06:37 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Phone } from 'lucide-react';
 import { Link } from 'react-router-dom';
