b8ddb9415b2e2b08ec3a7bae38c9777fe03a8ac1
iamsavinashreddy <iamsavinashreddy@gmail.com>
2025-04-09 12:23:38 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,11 +25,11 @@ const Navbar = () => {
         <nav
       className={`sticky top-0 z-50 transition-all duration-300 ${
         isScrolled
-          ? 'bg-white shadow-md py-1'
-          : 'bg-white/70 backdrop-blur-sm py-1'
+          ? "bg-white opacity-100 shadow-md py-1"
+          : "bg-white/40 backdrop-blur-sm opacity-40 py-2"
       }`}
     >
-      <div className="container mx-auto px-1 py-1">
+      <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
           {/* Logo */}  
           <Link 
